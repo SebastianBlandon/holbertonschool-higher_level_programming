f6ba1e354f0a8914cbedf79085ce41b00553e2a9
SebastianBlandon <blandonodz@gmail.com>
2022-05-05 16:16:54 -0500

    [UPDATED] 2-args.py, fixed output checkers

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     else:
         if (len(sys.argv) == 2):
             print("1 argument:\n1:", sys.argv[1])
-        elif (len(sys.argv) > 2):
+        else:
             print(len(sys.argv) - 1, "arguments:")
         for i in range(1, len(sys.argv)):
             print(i, ":", sys.argv[i])
