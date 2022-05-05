f9f91bcb3243f3e0a98e2d2e7639505168f3f70d
SebastianBlandon <blandonodz@gmail.com>
2022-05-05 11:47:42 -0500

    [ADDED] 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if (len(sys.argv) == 1):
+        print("0 arguments.")
+    elif (len(sys.argv) == 2):
+        print("1 argument:\n1:", sys.argv[1])
+    elif (len(sys.argv) > 2):
+        print(len(sys.argv) - 1, "arguments:")
+        for i in range(1, len(sys.argv)):
+            print (i, ":", sys.argv[i])
