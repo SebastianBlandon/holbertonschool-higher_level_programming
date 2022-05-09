c0e3cfb3bfdc42d88acb3b951f0f1d948722b573
SebastianBlandon <blandonodz@gmail.com>
2022-05-09 12:33:56 -0500

    [UPDATED] 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
     ce = "cC"
-    new_string = ''.join( x for x in my_string if x not in ce)
+    new_string = ''.join(x for x in my_string if x not in ce)
     return new_string
