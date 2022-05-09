7377be611aeb4960f032d95d6b61fab4887bf688
SebastianBlandon <blandonodz@gmail.com>
2022-05-09 12:33:06 -0500

    [UPDATED] 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if (my_string is None)
-        return None
     ce = "cC"
     new_string = ''.join( x for x in my_string if x not in ce)
     return new_string
