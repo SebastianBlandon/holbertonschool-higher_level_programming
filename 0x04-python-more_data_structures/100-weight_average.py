d1edac24ffbfba4524cb919de8c33abcada14ef7
SebastianBlandon <blandonodz@gmail.com>
2022-05-10 17:51:11 -0500

    [ADDED] 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    nume = 0
+    deno = 0
+    for tupl in my_list:
+        nume += (tupl[0] * tupl[1])
+        deno += tupl[1]
+    return nume / deno
