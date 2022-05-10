68dfbeb2d14c637e451b1ec5da5bf100f548e068
SebastianBlandon <blandonodz@gmail.com>
2022-05-09 19:12:53 -0500

    [UPDATED] 100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,5 +1,5 @@
-#define PY_SSIZE_T_CLEAN
-#include <Python.h>
+#include <listobject.h>
+#include <object.h>
 #include <stdio.h>
 /**
  * print_python_list_info - prints some basic info about Python lists.
