07c2a3c56fcd04d6e12e4397fc5f27675568ae6b
SebastianBlandon <blandonodz@gmail.com>
2022-05-12 15:42:45 -0500

    [UPDATED] 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,21 +5,10 @@ def roman_to_int(roman_string):
     flag = 0
     if ((type(roman_string) != str) or (roman_string is None)):
         return int_value
+    convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for i in range(len(roman_string)):
-        if roman_string[i] == "I":
-            int_string.append(1)
-        elif roman_string[i] == "V":
-            int_string.append(5)
-        elif roman_string[i] == "X":
-            int_string.append(10)
-        elif roman_string[i] == "L":
-            int_string.append(50)
-        elif roman_string[i] == "C":
-            int_string.append(100)
-        elif roman_string[i] == "D":
-            int_string.append(500)
-        elif roman_string[i] == "M":
-            int_string.append(1000)
+        if roman_string[i] in convert:
+            int_string.append(convert[roman_string[i]])
         else:
             return int_value
     for i in range(len(int_string)):
