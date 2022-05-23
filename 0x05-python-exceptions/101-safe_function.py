da9c26093044babeed6576bf20e545ac1aeff47f
SebastianBlandon <blandonodz@gmail.com>
2022-05-23 17:30:13 -0500

    [FIXED] 101-safe_function.py, ONE CHECK OUTPUT

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,8 +4,9 @@ import sys
 
 def safe_function(fct, *args):
     try:
-        result = fct(args[0], args[1])
+        result = fct(*args)
     except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
-        return None
-    return result
+        result = None
+    finally:
+        return result
