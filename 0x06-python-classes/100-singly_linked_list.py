8132cf8615b839105f58998918d06490edb60036
SebastianBlandon <blandonodz@gmail.com>
2022-05-24 20:46:16 -0500

    [UPDATED] 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,26 +6,48 @@
 
 
 class Node:
+    """ Class Node with attributes:
+
+    """
     def __init__(self, data, next_node=None):
+        """ __init__ initializes attributes of class
+            data (int): data of node
+            next_node (Node): next_node of a Node
+
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """ data property to retrieve of the data
+
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """ data property setting of the data
+            value (int): value to set
+
+        """
         if not type(value) is int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """ next_node property to retrieve the next_node
+
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """ next_node property to setting of the next_node
+            value (Node): pointer of Node
+
+        """
         if not (value is None or type(value) is Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
@@ -38,10 +60,21 @@ class Node:
 
 
 class SinglyLinkedList:
+    """ Class SinglyLinkedList
+
+    """
     def __init__(self):
+        """ __init__ initializes of attributes
+
+        """
         self.__head = None
 
     def __repr__(self):
+        """ __repr__ Returns a String with a representation of the state
+        of the object and this String must be interpreted without errors
+        by Python.
+
+        """
         temp = self.__head
         str_out = ""
         while temp:
@@ -52,6 +85,11 @@ class SinglyLinkedList:
         return str_out
 
     def sorted_insert(self, value):
+        """ sorted_insert insert the value in a new node
+
+            value (int): value to insert in node
+
+        """
         if not self.__head:
             self.__head = Node(value)
         else:
