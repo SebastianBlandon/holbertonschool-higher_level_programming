62807f5dcc9ac7a53ab19c0be9813a0140fdf3a3
SebastianBlandon <blandonodz@gmail.com>
2022-06-07 14:08:51 -0500

    [ADDED] 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This is the "BaseGeometry"  module.
+
+This module provides a simple BaseGeometry class.
+"""
+
+
+class BaseGeometry():
+    """ Empty class BaseGeometry that defines a base geometry
+
+        Args:
+            Nothing
+
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
