3de711c2db2777d7ebf75512e2ca9ea81e93b32c
SebastianBlandon <blandonodz@gmail.com>
2022-06-07 23:27:32 -0500

    [UPDATED] 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,13 +14,14 @@ class BaseGeometry():
 
     """
     def area(self):
-       raise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if (type(value) is not int):
             raise TypeError("{} must be an integer".format(name))
         if (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """ Empty class Rectangle that defines a rectangle
