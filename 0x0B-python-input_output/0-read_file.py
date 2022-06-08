89470c11a0a7d29a649739fc8d04219de293a61a
SebastianBlandon <blandonodz@gmail.com>
2022-06-08 16:42:53 -0500

    [UPDATED] 0-read_file.py, fix pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,8 @@
 
 
 def read_file(filename=""):
-    """ Function function that reads a text file (UTF8) and prints it to stdout:
+    """ Function function that reads a text file (UTF8)
+    and prints it to stdout:
 
         Args:
             file (file with str): input file
