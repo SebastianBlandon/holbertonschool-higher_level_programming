e1f54df5d22a68edb4a5dfca7d00eb2eb0f5984d
SebastianBlandon <blandonodz@gmail.com>
2022-06-08 20:17:03 -0500

    [UPDATED] 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import json
 """
     Function from_json_string(my_str)
 """
-
+import json
 
 def from_json_string(my_str):
     """ Function that returns an object (Python data structure)
