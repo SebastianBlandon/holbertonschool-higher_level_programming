38d1abd76d44c6f3c3c8f2a3dd37dac057a3aa24
SebastianBlandon <blandonodz@gmail.com>
2022-06-08 22:06:39 -0500

    [ADDED] 10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    Metohd Class Student
+"""
+
+
+class Student():
+    """
+        class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def properties(self):
+        return [i for i in self.__dict__.keys() if i[:1] != '_']
+
+    def to_json(self, attrs=None):
+        if attrs:
+            output = {}
+            for i in attrs:
+                for j in self.properties():
+                    if j is i:
+                        output[j] = self.__getattribute__(i)
+            return output
+        else:
+            return self.__dict__
