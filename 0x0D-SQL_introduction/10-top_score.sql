3b38928354a12435d991f783ded7c1d946d5d984
SebastianBlandon <blandonodz@gmail.com>
2022-07-12 19:50:09 -0500

    [ADDED] 9-full_creation.sql 10-top_score.sql files

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
+
+-- SQL introduction project
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
