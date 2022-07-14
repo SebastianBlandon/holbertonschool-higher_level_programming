9c5c23e4605ce8eed03f2a318bcac4ab25e9e151
SebastianBlandon <blandonodz@gmail.com>
2022-07-14 14:56:54 -0500

    [UPDATED] 15-comedy_only.sql file.

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- SQL more queries project
--- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter. 
+-- script that lists all Comedy shows in the database hbtn_0d_tvshows. 
 SELECT tv_shows.title
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
