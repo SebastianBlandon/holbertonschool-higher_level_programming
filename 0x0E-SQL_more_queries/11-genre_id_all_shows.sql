edf673639c9af5071c6c44d6943766dc0b32fd79
SebastianBlandon <blandonodz@gmail.com>
2022-07-13 20:35:12 -0500

    [ADDED] 11-genre_id_all_show.sql file.

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- SQL more queries project
--- script that lists all shows contained in the database hbtn_0d_tvshows. 
+-- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked. 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
