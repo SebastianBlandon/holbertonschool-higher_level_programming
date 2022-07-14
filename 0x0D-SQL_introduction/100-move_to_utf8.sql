1f02d2614c07d4ae6f83afa1332db77af41b5894
SebastianBlandon <blandonodz@gmail.com>
2022-07-13 21:15:28 -0500

    [ADDED] 100-move_to_utf8.sql file.

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,5 @@
+-- script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server. 
+-- SQL introduction project
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
