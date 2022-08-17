b59e0b31709fbd440ec7e2ff12448c7f1daa17f2
SebastianBlandon <blandonodz@gmail.com>
2022-08-17 12:04:16 -0500

    [ADDED] 3-starwars_title.js file

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id + '/';
+const axios = require('axios').default;
+
+// Make a request for a user with a given ID
+axios.get(url)
+  .then(function (response) {
+    // handle success
+    console.log(response.data.title);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
