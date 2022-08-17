7e2b622405367ee22e206db9a21914523018b176
SebastianBlandon <blandonodz@gmail.com>
2022-08-16 21:31:56 -0500

    [ADDED] 7-occurences file

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let occ = 0;
+  for (const i in list) {
+    if (searchElement == list[i]) {
+      occ += 1;
+    }
+  }
+  return occ;
+};
