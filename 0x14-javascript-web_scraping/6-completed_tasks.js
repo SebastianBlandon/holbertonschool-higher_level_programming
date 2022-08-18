a102b4edd3ea99d4ac8a72cbd4689a1b0924eced
SebastianBlandon <blandonodz@gmail.com>
2022-08-17 23:43:10 -0500

    [ADDED] 6-completed_tasks.js file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const axios = require('axios').default;
+const url = process.argv[2];
+
+axios.get(url)
+  .then(function (response) {
+    const count = {};
+    for (let i = 0; response.data[i]; i++) {
+      if (response.data[i].completed === true) {
+        if (isNaN(count[response.data[i].userId])) {
+          count[response.data[i].userId] = 1;
+        } else {
+          count[response.data[i].userId] += 1;
+        }
+      }
+    }
+    console.log(count);
+  });
