28d8c488826b17b2ac58ea2c82ff052587f9b90a
SebastianBlandon <blandonodz@gmail.com>
2022-08-30 20:20:22 -0700

    [UPDATED] 0-select_states.py file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Get all states
-# Write a script that lists all states from the database hbtn_0e_0_usa:
+# Write a script that lists all states
+# from the database hbtn_0e_0_usa:
 import sys
 import MySQLdb
 
