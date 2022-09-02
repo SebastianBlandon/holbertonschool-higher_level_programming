9150178d4387f85c1dd9935097f4ced6ce766bca
SebastianBlandon <blandonodz@gmail.com>
2022-09-02 18:32:02 +0000

    [UPDATED] 3-error_code.py file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-     Error code #0
+    Error code #0
 """
+import urllib.request
 from sys import argv
 
 if __name__ == "__main__":
-    import urllib.request
-   
     req = urllib.request.Request(argv[1])
     try:
         with urllib.request.urlopen(req) as response:
