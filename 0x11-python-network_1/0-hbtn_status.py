71e04d6078a48b4597e8de9a642c2781c9813beb
SebastianBlandon <blandonodz@gmail.com>
2022-09-02 11:34:15 -0500

    [UPDATED] 0-hbtn_status.py file

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    What's my status? #0
+"""
+
+if __name__=="__main__":
+    import urllib.request
+
+    r = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(r) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf8")))
