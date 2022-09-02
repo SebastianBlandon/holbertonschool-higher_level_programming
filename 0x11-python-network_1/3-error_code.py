8f154bd1a765b299b0dc9cd92ad5d5590896a7ff
SebastianBlandon <blandonodz@gmail.com>
2022-09-02 18:04:55 +0000

    [UPDATED] 3-error_code.py file

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    POST an email #0
+"""
+from sys import argv
+
+if __name__ == "__main__":
+    import urllib.request
+   
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.status))
