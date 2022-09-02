fda067cb6916d67e97166521f0cfc9b24f6b6dfc
SebastianBlandon <blandonodz@gmail.com>
2022-09-02 18:23:35 +0000

    [UPDATED] 4-hbtn_status.py file

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    What's my status? #1
+"""
+import requests
+
+r = requests.get('https://intranet.hbtn.io/status')
+print('Body response:')
+print('\t- type: {}'.format(type(r.text)))
+print('\t- content: {}'.format(r.text))
